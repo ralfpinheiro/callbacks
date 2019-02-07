234e2fb6b93fd746e96d17cd65905d87a9d9fdcf
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2019-02-07 19:22:37 +0000

    Initial callback using a for loop

---
+++ callback-waldo.js
@@ -0,0 +1,18 @@
+// find and logs the index of Waldo
+// print "Found Waldo at (index)!"
+
+
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === "Waldo") {
+      found(i);   // execute callback
+    }
+  }
+}
+
+function actionWhenFound(i) {
+  console.log("Found Waldo at index " + i + '!');
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
